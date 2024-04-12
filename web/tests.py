ba907e372f7323ee49a7c3cf0b8e6564cc0df228
shuhhan <shuhuanhan@163.com>
2024-04-12 15:38:21 +0800

    message7: Refactoring

--- web/tests.py
+++ web/tests.py
@@ -1,19 +1,12 @@
+from urllib import response
 from django.test import TestCase
 from django.urls import resolve
 from web.views import home_page #(1)
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
-class HomePageTest(TestCase):
-    
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/') #(2)
-        self.assertEqual(found.func, home_page) #(3)
-    
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest() #(4)
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+class HomePageTest(TestCase):  
+    def test_uses_home_template(self):
+        response = self.client.get('/') #(4)
+        self.assertTemplateUsed(response, 'home.html') #(5)
--- web/views.py
+++ web/views.py
@@ -32,4 +32,4 @@ def index(request):
     return render(request,"index.html",{"data":data})
 
 def home_page(request):
-    return HttpResponse("<html><title>To-Do lists</title></html>")
+    return render(request,"home.html")
