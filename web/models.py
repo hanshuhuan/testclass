e18697c5a4954328c7e196f21c9d6f45264b0612
shuhhan <shuhuanhan@163.com>
2024-04-23 14:29:49 +0800

    message 10:first model migration

--- web/models.py
+++ web/models.py
@@ -4,3 +4,6 @@ class UserInfo(models.Model):
     username = models.CharField(verbose_name="姓名",max_length=32)
     password = models.CharField(verbose_name="密码",max_length=32)
     email = models.EmailField(verbose_name="邮箱",max_length=32)
+
+class Item(models.Model):
+    text=models.TextField(default='')
